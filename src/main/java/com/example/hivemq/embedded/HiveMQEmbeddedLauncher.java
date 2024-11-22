8bc4deedd7804813d6e5e688dc8f1c7ef53ed4aa
Jan Hänel <j.haenel@gmx.net>
2024-11-22 22:28:34 +0100

    Initial commit

---
+++ src/main/java/com/example/hivemq/embedded/HiveMQEmbeddedLauncher.java
@@ -0,0 +1,18 @@
+package com.example.hivemq.embedded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HiveMQEmbeddedLauncher {
+
+	@Bean
+	public EmbeddedExtension2 embeddedExtension2() {
+		return new EmbeddedExtension2();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveMQEmbeddedLauncher.class, args);
+	}
+}
